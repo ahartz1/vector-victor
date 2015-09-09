4a5460a44accf4f134c6940600f767389131be8d
Adam Hartz <ahartz1@gmail.com>
2015-09-09 16:14:18 -0400

    Working: shape, vector_add, vector_sub, vector_sum, dot

--- linear_algebra.py
+++ linear_algebra.py
@@ -35,7 +35,14 @@ def vector_sum(*vectors):
 
 
 def dot(vec1, vec2):
-    pass
+    if shape(vec1)[0] == shape(vec2)[0]:
+        ret = [vec1[n] * vec2[n] for n in range(shape(vec1)[0])]
+        ret = sum(ret)
+        print(ret)
+    else:
+        raise ShapeException('Shape rule: the vectors must be the same size.')
+    return ret
+
 
 
 def vector_multiply(vec1, scalar):
