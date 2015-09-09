3eb58e230abcbfb9920a0f172e2dfd861ab7ba52
Adam Hartz <ahartz1@gmail.com>
2015-09-09 19:20:34 -0400

    Vector mean working.

--- linear_algebra.py
+++ linear_algebra.py
@@ -48,8 +48,15 @@ def vector_multiply(vec, scalar):
     return [scalar*e for e in vec]
 
 
-def vector_mean(vec):
-    pass
+def vector_mean(*vectors):
+    shape_expectation = shape(vectors[0])
+    equal_vectors = [shape(v) for v in vectors if shape_expectation == shape(v)]
+    if len(equal_vectors) != len(vectors):
+        raise ShapeException('Shape rule: the vectors must be the same size.')
+    columns = zip(*vectors)
+    ret = vector_sum(*vectors)
+    ret = vector_multiply(ret, 1/len(vectors))
+    return ret
 
 
 def magnitude(vec):
