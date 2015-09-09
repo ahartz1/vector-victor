7379121ee8cd80f2865eb90c824bc8ae38430db7
Adam Hartz <ahartz1@gmail.com>
2015-09-09 15:54:07 -0400

    Working vector sum

--- linear_algebra.py
+++ linear_algebra.py
@@ -10,9 +10,7 @@ def shape(vec):
 
 def vector_add(vec1, vec2):
     if shape(vec1)[0] == shape(vec2)[0]:
-        ret = [vec1[n] + vec2[n]           # collection
-               for n in range(shape(vec1)[0]) # iteration
-               ]
+        ret = [vec1[n] + vec2[n] for n in range(shape(vec1)[0])]
     else:
         raise ShapeException('Shape rule: the vectors must be the same size.')
     return ret
@@ -20,24 +18,20 @@ def vector_add(vec1, vec2):
 
 def vector_sub(vec1, vec2):
     if shape(vec1)[0] == shape(vec2)[0]:
-        ret = [vec1[n] - vec2[n]           # collection
-               for n in range(shape(vec1)[0]) # iteration
-               ]
+        ret = [vec1[n] - vec2[n] for n in range(shape(vec1)[0])]
     else:
         raise ShapeException('Shape rule: the vectors must be the same size.')
     return ret
 
 
 def vector_sum(*vectors):
-    # if shape(vec1)[0] == shape(vec2)[0]:
-    #     ret = [vec1[n] + vec2[n]           # collection
-    #            for n in range(shape(vec1)[0]) # iteration
-    #            ]
-    # else:
-    #     raise ShapeException('Shape rule: the vectors must be the same size.')
-    # return ret
-    pass
-
+    shape_expectation = shape(vectors[0])
+    equal_vectors = [shape(v) for v in vectors if shape_expectation == shape(v)]
+    if len(equal_vectors) != len(vectors):
+        raise ShapeException('Shape rule: the vectors must be the same size.')
+    ret = []
+    ret = [sum(c) for c in zip(*vectors)]
+    return ret
 
 
 def dot(vec1, vec2):
